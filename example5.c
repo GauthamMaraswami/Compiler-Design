dedcf01a0b719eacd6d9307c071e979169e13350
Gdarkstar <15co118.gautham@nitk.edu.in>
2018-01-10 14:38:20 +0530

    loops and arrays added

--- example5.c
+++ example5.c
@@ -2,6 +2,17 @@
 int main()
 {	
 	char c[10]="ab\qcd";
+	int a[100];
+	for(int n=0;n<10;++n)
+	{
+		scanf("%d",&a[i]);
+	}
+	int ctr=0;
+	while(ctr<10)
+	{
+		printf("%d",a[i]);
+	++ctr;	
+	}
 	printf("Compiler Design");	
 	return 0;
 }
